0dd615fcb1563a98dab6a39e070dccf2441fef7e
Fabiana <fabianacristina89@gmail.com>
2012-07-19 13:37:55 -0300

    revertendo

--- app/models/simulacao_produto.rb
+++ app/models/simulacao_produto.rb
@@ -1,8 +1,3 @@
 class SimulacaoProduto < ActiveRecord::Base
-  
-   
-  
-  attr_accessible :existe, :preco_compra, :icms, :ipi, :outros_impostos, :comissao, :frete, :outros_custos, :preco_calculado, :preco_vpsa, :produto_vpsa_id, :descricao 
-
- 
+  attr_accessible :existe, :aliquota_icms, :icms, :ip, :outros_custos, :preco_calculado, :preco_compra, :preco_vpsa, :produto_vpsa_id, :st,:descricao 
 end
