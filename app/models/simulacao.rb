e3d0ec8d10e091cd5169ba9cf4067c9e4e9cd99c
Fabiana <fabianacristina89@gmail.com>
2012-07-24 22:00:59 -0300

    retirando codigo desnecessario

--- app/models/simulacao.rb
+++ app/models/simulacao.rb
@@ -4,16 +4,5 @@ class Simulacao < ActiveRecord::Base
   attr_accessible :base, :margem_lucro,  :total_despesas,
    :total_receitas, :despesas_fixas;
 
-    def margem_lucro
-	    number_to_currency(attributes['margem_lucro'], :unit => "", :separator => ",", :delimiter => ".");
-	end
-	def total_despesas
-	    number_to_currency(attributes['total_despesas'], :unit => "", :separator => ",", :delimiter => ".");
-	end
-	def total_receitas
-	    number_to_currency(attributes['total_receitas'], :unit => "", :separator => ",", :delimiter => ".");
-	end
-	def despesas_fixas
-	    number_to_currency(attributes['despesas_fixas'], :unit => "", :separator => ",", :delimiter => ".");
-	end
+   
 end
