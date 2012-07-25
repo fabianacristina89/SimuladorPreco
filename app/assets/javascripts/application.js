23bfbf9245949924ed56a3c6e55433b18a5cf91a
Fabiana <fabianacristina89@gmail.com>
2012-07-24 21:28:00 -0300

    filtro

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -109,3 +109,15 @@ function parseCurrencyToFloat(vlr){
 		return parseFloat(vlr);
 	}
 }
+
+function filter (phrase, _id, cellNr){
+  var suche = phrase.value.toLowerCase();
+  var table = document.getElementById(_id);
+  var ele;
+  for (var r = 1; r < table.rows.length; r++){
+        ele = table.rows[r].cells[cellNr].innerHTML.replace(/<[^>]+>/g,"");
+        if (ele.toLowerCase().indexOf(suche)>=0 )
+    		table.rows[r].style.display = '';
+        else table.rows[r].style.display = 'none';
+  }
+}
